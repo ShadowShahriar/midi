f8cba6761199a85c35d97241fb3b901482ac736d
Shahriar <shadowshahriar.dev@gmail.com>
2021-09-17 00:11:53 +0600

    First commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+<!-- @format -->
+
+# midi
+
+Test
---
+++ index.js
@@ -0,0 +1,44 @@
+/** @format */
+
+const path = require('path')
+const fs = require('fs')
+
+const midi_dir = '.mid'
+const json_file = 'index.json'
+
+const directory = path.resolve(__dirname, midi_dir)
+const output = path.resolve(__dirname, json_file)
+const entries = fs.readdirSync(directory, { withFileTypes: true })
+
+const array = []
+
+for (const entry of entries) {
+	if (entry.isFile() && entry.name.slice(-4) === '.mid') {
+		array.push(entry.name)
+	}
+}
+
+const object = {
+	directory: midi_dir,
+	length: array.length,
+	entries: array
+}
+
+const json = JSON.stringify(object, null, '\t')
+console.log(json)
+
+// delete existing JSON
+try {
+	fs.unlinkSync(output)
+} catch (e) {}
+
+try {
+	fs.writeFileSync(output, json, {
+		encoding: 'utf-8',
+		mode: 0o666
+	})
+
+	console.log('\n', '🎉 Wrote to JSON')
+} catch (e) {
+	console.log(e)
+}
